c9001bd1de51d857f928bedc2c4f1e91285ce75c
Fernando Nerio Jr <fernandonerio90@gmail.com>
2024-10-26 17:52:30 -0600

    refactor: minimize const variables and replace it with html ids

--- script/base.js
+++ script/base.js
@@ -3,18 +3,18 @@
 REVEAL TEXT FEATURE ON MAIN SECTION
 \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 */
-const mainFirstReveal = document.querySelector('.main-first-reveal');
-const mainSecondReveal = document.querySelector('.main-second-reveal');
-const mainThirdReveal = document.querySelector('.main-third-reveal');
+const textRevealMain = document.querySelector('.main-first-reveal');
 const revealArray = [mainFirstReveal, mainSecondReveal, mainThirdReveal];
-
 // Reveals the headings/paragraph from main section
 window.addEventListener('scroll', function() {
-  const mainSectionPosition = triggerTextReveal.getBoundingClientRect();
+  const mainSectionPosition = textRevealMain.getBoundingClientRect();
 
   revealArray.forEach(function(activate) {
   if(mainSectionPosition.top < window.innerHeight && mainSectionPosition.bottom >= 0 ){
       activate.classList.add('active');
+    }
+    else{
+      activate.classList.remove('active');
     }
   });
 });
@@ -24,11 +24,10 @@ window.addEventListener('scroll', function() {
 NAVIGATION FEATURES
 \\\\\\\\\\\\\\\\\\\\\\\\\\\\
 */
-const hamburgerButton = document.querySelector('.hamburger-menu');
-const logoLink = document.querySelector('.logo');
+
+const hamburgerLines = document.querySelectorAll('section.hamburger-menu > span');
 const navArray = [mainNavigation, menuLinks];
 const allLinks = document.querySelectorAll('.links');
-const hamburgerLines = document.querySelectorAll('section.hamburger-menu > span');
 
 
 // Clicking Hamburger button will drop menus
@@ -47,10 +46,15 @@ allLinks.forEach(function(links){
   });
 });
 
-// Clicking on logo removes each "dropdown" classname
+/* Clicking on logo removes each "dropdown" 
+  classname as well as the active hamburger lines
+*/
 logoLink.addEventListener('click', function() {
   navArray.forEach(function(dropdown) {
     dropdown.classList.toggle('dropdown');
+  });
+  hamburgerLines.forEach(function(inactive) {
+    inactive.classList.remove('active');
   });
 });
 
@@ -68,6 +72,22 @@ allLinks.forEach(function(links) {
       remove.classList.remove('active');
     });
   });
+});
+
+
+/*
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+MAIN SEACTION FEATURES
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+*/
+
+// Hover over the learn more button displays the right arrow icon
+mainLearnMoreBtn.addEventListener('mouseover', function() {
+  mainArrowIcon.classList.add('hovered');
+});
+
+mainLearnMoreBtn.addEventListener('mouseout', function() {
+  mainArrowIcon.classList.remove('hovered');
 });
 
 
